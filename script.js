33b6e73d6f785e534051f99732efd6c2f63dde2b
ciri-stowell <ciri.stowell@touro.edu>
2018-01-10 16:47:32 -0500

    stuff

---
+++ script.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+  
+  var menu = $(".nav-container .nav");
+  $(".sub-button").click(function() {
+    var subMenu = $(this).siblings("ul");
+    subMenu.show();
+
+    menu.animate({
+      "margin-left" : "-="+menu.width()+"px"
+    }, 'linear');
+
+  });
+  
+  var backButton = $(".nav-container .nav li.title > .go-back");
+
+backButton.click(function() {
+    var subMenu = $(this).parent().parent();
+    menu.animate({
+      "margin-left" : "+="+menu.width()+"px"
+    }, "linear", function() {
+        subMenu.hide();
+    });
+  });
+
+  $("#main-menu #about").click(function() {
+    $("#main-menu #about").hide();
+    $(".nav-container").show();
+    $(".nav-dropdown ul").each(function() {
+    });
+
+  });
+
+
+$(".page").click(function() {
+  var title = $(this).text();
+   $("#main-menu").hide();
+    $("p.page-title").replaceWith("<p class='page-title'>" + title + "</p>");
+    $("#hamburger").show();
+  $("#menu-button-x").hide();
+});
+
+$("#hamburger").click(function() {
+  $("#main-menu").toggle();
+  $("#hamburger").hide();
+  $("#menu-button-x").show();
+});
+
+$("#menu-button-x").click(function() {
+  $("#main-menu").toggle();
+  $("#hamburger").show();
+  $("#menu-button-x").hide();
+});
+
+
+$("#back-to-main").click(function() {
+  $("#main-menu #about").show();
+  $(".nav-container").hide();
+});
+
+// $("#main-menu a").click(function() {
+//   $(".menu-button").hide();
+//   $(".menu-button-x").show();
+//   $('.nav-dropdown ul').each(function() {
+//     if($(this).css("display") === "block") {
+//       $(this).hide();
+//       menu.css('margin-left', '0')
+//     }
+//   });
+
+//   $("#main-menu").toggle();
+//   $(".nav-container").toggle();
+
+// });
+
+
+});
